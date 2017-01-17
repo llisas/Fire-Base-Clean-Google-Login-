e4d24f7572de767ade7e8f0188bcf4c1afe4ba32
jesus c <jesuscuevasrodulfo@gmail.com>
2017-01-17 17:54:03 +0100

    Removed not used imports

--- app/src/main/java/com/jesuscuevas/app/getmyfilm/ui/activities/HeadActivy.java
+++ app/src/main/java/com/jesuscuevas/app/getmyfilm/ui/activities/HeadActivy.java
@@ -1,14 +1,11 @@
 package com.jesuscuevas.app.getmyfilm.ui.activities;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
 import com.jesuscuevas.app.getmyfilm.R;
 import com.jesuscuevas.app.getmyfilm.base.BaseActivity;
 
 public class HeadActivy extends BaseActivity {
 
-   
+
     @Override
     protected void getPresenter() {
 
--- app/src/main/java/com/jesuscuevas/app/getmyfilm/ui/activities/MainActivity.java
+++ app/src/main/java/com/jesuscuevas/app/getmyfilm/ui/activities/MainActivity.java
@@ -2,34 +2,20 @@ package com.jesuscuevas.app.getmyfilm.ui.activities;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
-
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
 import com.jesuscuevas.app.getmyfilm.base.BaseActivity;
 import com.jesuscuevas.app.getmyfilm.R;
 import com.jesuscuevas.app.getmyfilm.ui.mvp.helper.LoginHelper;
 import com.jesuscuevas.app.getmyfilm.ui.mvp.presenters.MainActivityPresenter;
 import com.jesuscuevas.app.getmyfilm.ui.mvp.presenters.MainActivityPresenterImpl;
 import com.jesuscuevas.app.getmyfilm.ui.mvp.views.MainActivityView;
-import com.jesuscuevas.app.getmyfilm.utils.Constans;
-
 import butterknife.OnClick;
-
 import static com.jesuscuevas.app.getmyfilm.utils.Constans.RC_SIGN_IN;
+
 
 public class MainActivity extends BaseActivity implements
         GoogleApiClient.OnConnectionFailedListener, MainActivityView, LoginHelper {
